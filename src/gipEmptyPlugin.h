2b5d400b7f0910819575a35dda3d263f9bc972b4
Metehan Gezer <irrelevantdev@outlook.com>
2025-07-29 19:37:43 +0300

    initial commit

---
+++ src/gipEmptyPlugin.h
@@ -0,0 +1,22 @@
+/*
+ * gipEmptyPlugin.h
+ *
+ *  Created on: Mar 11, 2023
+ *      Author: Noyan Culum
+ *      Created by: kayra
+ */
+
+#ifndef SRC_GIPEMPTYPLUGIN_H_
+#define SRC_GIPEMPTYPLUGIN_H_
+
+#include "gBasePlugin.h"
+#include "znet/znet.h"
+
+class gipEmptyPlugin : public gBasePlugin{
+public:
+	gipEmptyPlugin();
+	virtual ~gipEmptyPlugin();
+
+};
+
+#endif /* SRC_GIPEMPTYPLUGIN_H_ */
