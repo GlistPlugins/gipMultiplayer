7d7d4a0d4f5a76429f39cd1f7439f8a2a89142b1
Muhammet Furkan Demir <mfurkandemir554223@gmail.com>
2025-08-07 17:29:28 +0300

    Update gipClientHandler.h

--- src/gipClientHandler.h
+++ src/gipClientHandler.h
@@ -2,7 +2,7 @@
 * gipClientHandler.h
  *
  *  Created on: August 5, 2025
- *      Created by: Yusuf Ustaoglu
+ *      Authors: Yusuf Ustaoglu, Muhammet Furkan Demir
  */
 
 #ifndef GIPCLIENTHANDLER_H
