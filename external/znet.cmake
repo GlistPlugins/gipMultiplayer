2f5f9610229b4241da93d5aa5e700ce6ad2a4ae1
irrld <irrelevantdev@outlook.com>
2026-08-21 01:11:59 +0300

    bump znet for the multi address punch candidates

--- external/znet.cmake
+++ external/znet.cmake
@@ -14,7 +14,7 @@ endif()
 
 set(ZNET_GIT_REPOSITORY "https://github.com/teoncreative/znet.git"
 		CACHE STRING "Git repository to fetch znet from")
-set(ZNET_GIT_TAG "202a83c2e634a016c79e3073a0cbe9d23aec63ba"
+set(ZNET_GIT_TAG "1ca8e11a01d9c3e0b628e65d21787063156b898c"
 		CACHE STRING "znet commit, tag or branch to build against")
 set(ZNET_ZSTD_GIT_TAG "48c0ed73625272cb7445183b5e256b5d0a130316"
 		CACHE STRING "zstd commit to build znet's compression against")
